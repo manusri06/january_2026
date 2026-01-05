0bc4170dbd234e1db9a9b0a12d2b166e67746980
V. Manusri <velpula.manusri06@gmail.com>
2026-01-05 06:53:27 +0530

    Time: 699 ms (59.81%), Space: 0B (100%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -1,5 +1,4 @@
 
 SELECT user_id, name, mail
 FROM Users
-WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\\.com$'
-AND mail LIKE BINARY '%@leetcode.com';
+WHERE mail collate utf8mb3_bin REGEXP '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\\.com$';
