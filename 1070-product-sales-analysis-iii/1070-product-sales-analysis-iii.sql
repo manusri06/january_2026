833211f2c31f102635d0ab529a83eee8d12217be
V. Manusri <velpula.manusri06@gmail.com>
2026-02-01 11:24:11 +0530

    Time: 775 ms (25.88%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select s.product_id, s.year as first_year, s.quantity, s.price
+select s.product_id, t.first_year, s.quantity, s.price
 from Sales s
 join (
     select product_id, min(year) as first_year
