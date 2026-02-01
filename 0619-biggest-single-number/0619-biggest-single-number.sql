ba141e16000b11cc96398f79925073a5b8bfdf5b
V. Manusri <velpula.manusri06@gmail.com>
2026-02-01 15:15:36 +0530

    Time: 373 ms (91.03%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,18 +1,10 @@
-/* # Write your MySQL query statement below
+# Write your MySQL query statement below
 select max(num) as num
 from MyNumbers
-where num in(
+where num in (
     select num
     from MyNumbers
     group by num
     having count(*) = 1
-);
- */
 
- select max(num) as num
- from MyNumbers m1
- where 1 = (
-    select count(*)
-    from MyNumbers m2
-    where m1.num = m2.num
- )
+)
